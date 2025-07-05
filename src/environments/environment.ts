438718e3782f78ea0b5ba97e7c4230423f203cad
rohit.sathwara1996@gmail.com <rohit@1996>
2025-07-05 12:11:35 +0530

    - changes envirment url link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,11 @@
+﻿// export const environment = {
+//  apiUrl: 'http://127.0.0.1:8000/docs'
+// };
+
+
+// src/environments/environment.ts
 export const environment = {
-  apiUrl: 'http://127.0.0.1:8000/api'
+  apiUrl: 'https://document-world-api.onrender.com/api'
 };
+
 
